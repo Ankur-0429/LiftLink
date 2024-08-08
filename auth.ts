65cafff28f98d20f1d2a87bbe621de186eb1f30f
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-08-08 16:02:31 -0700

    modified schema file

--- auth.ts
+++ auth.ts
@@ -9,15 +9,23 @@ export const {
 } = NextAuth({
   ...authConfig,
   callbacks: {
-    async jwt({token}) {
-        return token;
+    async jwt({ token }) {
+      return token;
     },
-    async session({token, session}) {
-        if (token.sub && session.user) {
-            session.user.id = token.sub;
-        }
-        return session;
-    }
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
   },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
