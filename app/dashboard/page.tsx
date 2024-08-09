996e9321b89758a5fa8610b5b973adf50b67fd7f
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-08-09 01:26:44 -0700

    initial setup for channel component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,18 @@
+import Channel from "@/components/channel";
+
+const memberData = () => {
+  const data = [];
+  for (let i = 0; i < 4; i++) {
+      data.push({name: "Charles", image: `https://i.pravatar.cc/${i+70}`})
+  }
+  return data;
+}
 
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className="max-w-screen-lg mx-auto">
+          <Channel owner={{name: "Ankur Ahir", image: "false"}} limit={5} members={memberData()} createdAt={new Date("2024-08-08T07:25:32.653Z")} description="Looking for people to move to slugWatch 2022. If anyone wants to carpool with me at cameron way, feel free to message me. Looking for anyone interested." ifRequestedAlready={false} />
+        </div>
     )
 };
 
---
+++ components/channel/index.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import moment from "moment";
+import { EllipsisVertical } from "lucide-react";
+import { Button } from "../ui/button";
+import { useState } from "react";
+
+type UserType = {
+  name?: string;
+  image?: string;
+};
+
+interface ChannelInterface {
+  owner: UserType;
+  members: UserType[];
+  limit: number;
+  createdAt: Date;
+  description: string;
+  ifRequestedAlready: boolean;
+}
+
+const Channel = ({
+  owner,
+  members,
+  limit,
+  createdAt,
+  description,
+  ifRequestedAlready,
+}: ChannelInterface) => {
+  const [ifRequested, setIfRequested] = useState(ifRequestedAlready);
+  const toggleRequest = () => {
+    setIfRequested(!ifRequested);
+  }
+  return (
+    <>
+      <div className="p-4">
+        <div className="flex justify-between items-center">
+          <div className="flex gap-x-2 items-center">
+            <Avatar className="h-12 w-12">
+              <AvatarImage src={owner.image} alt={owner.name} />
+              <AvatarFallback>
+                {owner.name?.charAt(0).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <div className="leading-none">
+              <p className="font-semibold cursor-pointer hover:underline">
+                {owner.name}
+              </p>
+              <span className="text-muted-foreground text-xs">
+                {moment(createdAt).fromNow()}
+              </span>
+            </div>
+          </div>
+          <div className="text-muted-foreground">
+            <EllipsisVertical size={20} />
+          </div>
+        </div>
+        <div className="ml-12">
+          <div className="my-3 flex items-center gap-2">
+            <div className="flex -space-x-3 *:ring *:ring-white">
+              {members.map((e) => {
+                return (
+                  <Avatar>
+                    <AvatarImage src={e.image} alt={e.name} />
+                    <AvatarFallback>
+                      {e.name?.charAt(0).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                );
+              })}
+            </div>
+            <p className="text-muted-foreground">
+              {limit - members.length}{" "}
+              {limit - members.length === 1 ? "person" : "people"} left
+            </p>
+          </div>
+          <p className="mb-3">{description}</p>
+          {ifRequested ? (
+            <Button variant="secondary" className="w-32" onClick={toggleRequest}>Requested</Button>
+          ) : (
+            <Button onClick={toggleRequest} className="w-32">Request to Join</Button>
+          )}
+        </div>
+      </div>
+      <Separator />
+    </>
+  );
+};
+
+export default Channel;
