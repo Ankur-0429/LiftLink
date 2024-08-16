b9d69529e9ef6f56280d4ddb90ee12ba8574b2cb
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-08-16 14:07:37 -0700

    moved to page.tsx

--- app/dashboard/channel/[channelid]/page.tsx
+++ app/dashboard/channel/[channelid]/page.tsx
@@ -1,7 +1,49 @@
 import MessageChannel from "./MessageChannel";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import db from "@/lib/db";
+import "@/app/globals.css";
 
-const ChannelPage = ({ params }: {params: {channelid: string}}) => {
+const inter = Inter({ subsets: ["latin"] });
+
+type Props = {
+  params: { channelid: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({
+  params,
+}: Props): Promise<Partial<Metadata>> {
+  const id = params.channelid;
+  try {
+    const data = await db.channel.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+      select: {
+        owner: {
+          select: {
+            name: true,
+          },
+        },
+        description: true,
+      },
+    });
+
+    return {
+      title: `Join ${data && data.owner.name}'s Carpool`,
+      description: data && data.description,
+    };
+  } catch (error) {
+    console.error("An error occurred while fetching the channel data:", error);
+    return {
+      title: "Error Loading Channel",
+    };
+  }
+}
+
+const Page = ({ params }: {params: {channelid: string}}) => {
   return <MessageChannel channelid={params.channelid} />;
 };
 
-export default ChannelPage;
+export default Page;
