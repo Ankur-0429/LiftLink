0404e94dd6e8734cac6bd81f7f3903b5287ebdf9
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-08-16 13:51:40 -0700

    made all page and layout peices server side

--- app/dashboard/channel/[channelid]/page.tsx
+++ app/dashboard/channel/[channelid]/page.tsx
@@ -1,50 +1,8 @@
-import type { Metadata, ResolvingMetadata } from "next";
 import MessageChannel from "./MessageChannel";
-import db from "@/lib/db";
 
 type Props = {
   params: { channelid: string };
-  searchParams: { [key: string]: string | string[] | undefined };
 };
-
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const id = params.channelid;
-  try {
-    const data = await db.channel.findUnique({
-      where: {
-        id: parseInt(id),
-      },
-      select: {
-        owner: {
-          select: {
-            name: true,
-          },
-        },
-        description: true,
-      },
-    });
-    const previousImages = (await parent).openGraph?.images || [];
-    
-    return {
-      title: `Join ${data && data.owner.name}'s Carpool`,
-      description: data && data.description,
-      openGraph: {
-        images: [...previousImages],
-      },
-    };
-  } catch (error) {
-    console.error("An error occurred while fetching the channel data:", error);
-    return {
-      title: "Error Loading Channel",
-      openGraph: {
-        images: [],
-      },
-    };
-  }
-}
 
 const ChannelPage = ({ params }: Props) => {
   return <MessageChannel channelid={params.channelid} />;
